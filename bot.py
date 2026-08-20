ea4716450cb854636c5eebb092b7f2f12519c770
llayth2002-svg <llayth2002@gmail.com>
2026-08-20 19:07:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,8 @@
+import os, requests
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SE_USER = os.getenv("SIGHTENGINE_USER")
+SE_SECRET = os.getenv("SIGHTENGINE_SECRET")
+
 def check_image_ai(img_bytes: bytes) -> bool:
     if not SE_USER or not SE_SECRET:
         return False
@@ -5,22 +10,19 @@ def check_image_ai(img_bytes: bytes) -> bool:
         r = requests.post(
             "https://api.sightengine.com/1.0/check.json",
             data={
-                'models': 'nudity,wad,gore,offensive,weapon,drugs',
+                'models': 'nudity,wad,gore,offensive,weapon,drugs,violence',
                 'api_user': SE_USER,
                 'api_secret': SE_SECRET
             },
             files={'media': img_bytes}
         )
         j = r.json()
-        # gore = دم وعنف وقتل
-        # weapon = سلاح وارهاب
-        # drugs = مخدرات
-        # wad = عنف
         if j.get('gore',{}).get('prob',0) > 0.3: return True
         if j.get('weapon',0) > 0.3: return True
         if j.get('drugs',0) > 0.3: return True
         if j.get('violence',0) > 0.3: return True
         if j.get('nudity',{}).get('raw',0) > 0.3: return True
+        if j.get('offensive',{}).get('prob',0) > 0.3: return True
         return False
     except:
         return False
